52e6e32e50f2843ef34b0a9459b62bfbd4b1ef4a
bit <bit@DESKTOP-8PO60DE>
2018-06-08 11:53:10 +0900

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,8 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println( "Hello World");
+		// github test
+		System.out.println( "Hello World!!!");
 
 	}
 
